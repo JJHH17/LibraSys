b6c9b0ecb03edb960c04d1c2ea8ff1fa738ba69b
James Hatfield <james.hatfield17@icloud.com>
2025-08-12 16:26:12 +0100

    Added initial program loop

--- src/App/Main.java
+++ src/App/Main.java
@@ -1,7 +1,28 @@
 package App;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Welcome to LibraSys");
+		boolean active = true;
+		
+		while (active) {
+			System.out.println("1. Create Account 2. Login 3. Exit");
+			int userInput = scanner.nextInt();
+			
+			switch (userInput) {
+				case 1:
+					break;
+					
+				case 2:
+					break;
+					
+				case 3:
+					active = false;
+					break;
+			}
+		}
 	}
 }
